5a715814d43fd2068a1854e0cda19eae4f014e15
Achmat Fauzi <achmatfauzi86@gmail.com>
2024-07-22 04:24:07 +0700

    Update ably.ts

--- src/plugins/ably.ts
+++ src/plugins/ably.ts
@@ -1,19 +1,21 @@
 import Ably from "ably";
 
 export default defineNuxtPlugin(() => {
-  const config = useRuntimeConfig()
-  let ably = new Ably.Realtime({
-    key: config.app.ablyAPIKey,
-    clientId : 'test-nuxt-ably', //optional
-});
+  if(import.meta.client) {
+    const config = useRuntimeConfig()
+    let ably = new Ably.Realtime({
+      key: config.app.ablyAPIKey,
+      clientId : 'test-nuxt-ably', //optional
+    });
 
-ably.connection.once("connected", () => {
-  console.log("Connected to Ably!")
-})
+    ably.connection.once("connected", () => {
+      console.log("Connected to Ably!")
+    })
 
-  return {
-    provide: {
-      ably: ably,
-    },
-  };
+    return {
+      provide: {
+        ably: ably,
+      },
+    };
+  }
 })
